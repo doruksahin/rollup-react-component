44c547ca7be3424ee9c0a854cabcb6161713f0a2
doruk <doruksahin98@gmail.com>
2023-06-07 20:42:29 +0300

    init

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface GridProps {
+  label: string;
+}
+
+const Grid = (props: GridProps) => {
+  return <div>This is a Grid</div>;
+};
+
+export default Grid;
